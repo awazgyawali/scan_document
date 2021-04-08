3e8b49ba0b3953c4483436c1562119c2e597f12d
Aawaz Gyawali <awazgyawali@gmail.com>
2021-04-08 15:39:30 +0545

    Hotfixes

--- lib/scan_document.dart
+++ lib/scan_document.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:scan_document/images_post_processing.dart';
 
 Future<File> scanDocument(BuildContext context) async {
